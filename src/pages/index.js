dec32f2f72f7fc34f069d5cc5efba4ca56c58589
jaehanjung <wjdwogks23@gmail.com>
2018-03-15 18:52:38 +0900

    git test

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ class BlogList extends React.Component {
         <div className="section section2 clearfix">
           <div className="card-more content">
             { edges.map((item, index)=>{
-              return (<PostCard key={`postcard${index}`} title={item.node.frontmatter.title} content={item.node.excerpt} link={item.node.fields.slug}/>)
+              return (<PostCard key={`postcard${index}s`} title={item.node.frontmatter.title} content={item.node.excerpt} link={item.node.fields.slug}/>)
             })}
           </div>
         </div>
